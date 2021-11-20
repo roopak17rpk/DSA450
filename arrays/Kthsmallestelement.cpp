8cbeaf9cbda127421432835f3fe3a0b2d880f019
Roopak Gupta <51106422+roopak17rpk@users.noreply.github.com>
2021-11-20 11:13:49 +0530

    Create Kthsmallestelement.cpp

---
+++ arrays/Kthsmallestelement.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> maxh;
+        for(int i = 0 ; i <=r ; i++)
+        {
+            maxh.push(arr[i]);
+            if(maxh.size() > k)
+            maxh.pop();
+        }
+        return maxh.top();
+    }
+};
